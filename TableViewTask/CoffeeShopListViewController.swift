bd7d49a4bfd40627778bfd82ed3835cdd74ba22d
Tiara <tiaranurulh@gmail.com>
2022-09-22 22:59:52 +0700

    second commit

--- TableViewTask/CoffeeShopListViewController.swift
+++ TableViewTask/CoffeeShopListViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class CoffeeShopListViewController: UIViewController, UITableViewDataSource {
+class CoffeeShopListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     
@@ -19,12 +19,17 @@ class CoffeeShopListViewController: UIViewController, UITableViewDataSource {
     
     var coffeeshopTypes = ["Coffeeshop / Cafe", "Tea House", "Cafe Shop", "Home Coffee", "Coffeeshop / Cafe", "Tea House", "Cafe", "Home Coffee", "Bakery / Cafe", "Tea House / Cafe", "Coffeeshop / Bakery", "Coffeeshop / Milk House", "Coffeeshop / Cafe", "Bakery", "Warmindo"]
     
+    var favoritedCoffeeshops: [Int] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.dataSource = self
-        
+        tableView.delegate = self
     }
+    
+    // MARK: - UITableViewDataSource
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return coffeeshopNames.count
     }
@@ -37,8 +42,75 @@ class CoffeeShopListViewController: UIViewController, UITableViewDataSource {
         cell.addressLabel.text = coffeeshopLocations[indexPath.row]
         cell.categoryLabel.text = coffeeshopTypes[indexPath.row]
         
+        if favoritedCoffeeshops.contains(where: { index in
+            return index == indexPath.row
+        }) {
+            cell.favoriteImageView.isHidden = false
+        } else {
+            cell.favoriteImageView.isHidden = true
+        }
+
         return cell
     }
+    
+    //MARK: - Helper Functions
+    
+    func reserveTable(_ index: Int) {
+        let alert = UIAlertController(title: "Not Available yet", message: "Sorry this feature is not available yet. \nPlease try again later", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alert, animated: true)
+    }
+    
+    func toogleFavorite(_ index: Int) {
+       
+        if favoritedCoffeeshops.contains(index) {
+            if let index = favoritedCoffeeshops.firstIndex(of: index) {
+                favoritedCoffeeshops.remove(at: index) }
+        } else {
+            favoritedCoffeeshops.append(index)
+        }
+        
+        // refresh whole cell
+        // tableView.reloadData()
+        
+        // refresh selected rows
+        tableView.reloadRows(at: [IndexPath(row: index, section: 0)], with: .none)
+        
+    }
+    
+    // MARK: - UITableViewDelegate
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let actionSheet = UIAlertController(title: "What do you want to do?", message: nil, preferredStyle: .actionSheet)
+        
+        actionSheet.addAction(UIAlertAction(title: "Reserve a table", style: .default, handler: { alert in
+            self.reserveTable(indexPath.row)
+        }))
+        
+        if favoritedCoffeeshops.contains(where: { index in
+            return index == indexPath.row }) {
+            actionSheet.addAction(UIAlertAction(title: "Remove from favorite", style: .default, handler: { _ in
+                self.toogleFavorite(indexPath.row)
+            }))
+        } else {
+            actionSheet.addAction(UIAlertAction(title: "Mark as favorite", style: .default, handler: { _ in
+            self.toogleFavorite(indexPath.row)
+            }))
+        }
+//        actionSheet.addAction(UIAlertAction(title: "Mark as favorite", style: .default, handler: { _ in
+//            self.toogleFavorite(indexPath.row)
+//
+//        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        present(actionSheet, animated: true)
+    }
+    
+    
     
     
 }
--- TableViewTask/coffeeShopViewCell.swift
+++ TableViewTask/coffeeShopViewCell.swift
@@ -13,6 +13,7 @@ class coffeeShopViewCell: UITableViewCell {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var addressLabel: UILabel!
     @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var favoriteImageView: UIImageView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
